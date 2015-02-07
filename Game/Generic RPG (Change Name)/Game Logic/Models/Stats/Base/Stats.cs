c5ccdcd6e08f3bec9bb7122bb2fa4196b9cfd9e2
Emil-Petrov <Emil.petrov95@mail.bg>
2015-02-07 13:23:52 +0200

    Deleted Useless Game folder

---
+++ Game/Generic RPG (Change Name)/Game Logic/Models/Stats/Base/Stats.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Game.Models.Stats
+{
+    abstract class Stats
+    {
+        private string _name;
+        private int _value;
+
+        protected Stats(string name,int value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name
+        {
+            get { return this._name; }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Stats name cannot be empty");
+                }
+            }
+        }
+
+        public int Value
+        {
+            get { return this._value; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Stats value cannot be negative");
+                }
+                this._value = value;
+            }
+        }
+    }
+}
