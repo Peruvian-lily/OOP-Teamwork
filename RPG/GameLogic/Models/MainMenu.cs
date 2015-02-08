bad8f61a60cf327ebd0d6e2b0e8397447c81cfe2
krastoss <servicemail945@gmail.com>
2015-02-09 01:24:23 +0200

    updated Menu

--- RPG/GameLogic/Models/MainMenu.cs
+++ RPG/GameLogic/Models/MainMenu.cs
@@ -16,7 +16,8 @@ namespace RPG.GameLogic.Models
         public MainMenu()
         {
             main.Add(new GUIElement("Overlays\\Menu\\main_menu_background_under_buttons"));
-          main.Add(new GUIElement("Overlays\\Menu\\new_game_plain"));
+            main.Add(new GUIElement("Overlays\\Menu\\new_game_plain"));
+            main.Add(new GUIElement("Overlays\\Menu\\exit_plain"));
         }
 
         public void LoadContent(ContentManager content)
@@ -24,14 +25,19 @@ namespace RPG.GameLogic.Models
             foreach (GUIElement element in main)
             {
                 element.LoadContent(content);
-                element.CenterElement(Game1.ScreenHeight,Game1.ScreenWidth);
+                element.CenterElement(Game1.ScreenHeight, Game1.ScreenWidth);
+                element.ClickEvent += OnClick;
             }
-           main.Find(x => x.AssetName == "Overlays\\Menu\\new_game_plain").MoveElement(0,-100);
+            main.Find(x => x.AssetName == "Overlays\\Menu\\new_game_plain").MoveElement(0, -100);
+            main.Find(x => x.AssetName == "Overlays\\Menu\\exit_plain").MoveElement(0, 100);
         }
 
         public void Update()
         {
-            
+            foreach (GUIElement element in main)
+            {
+                element.Update();
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -39,6 +45,14 @@ namespace RPG.GameLogic.Models
             foreach (GUIElement element in main)
             {
                 element.Draw(spriteBatch);
+            }
+        }
+
+        public void OnClick(string element)
+        {
+            if (element == "Overlays\\Menu\\new_game_plain")
+            {
+                //Play the Game
             }
         }
     }
