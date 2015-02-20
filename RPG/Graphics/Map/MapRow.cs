dc437815737053387788978b16794b5c16d300fe
krastoss <servicemail945@gmail.com>
2015-02-20 19:16:57 +0200

    map - work in progress

---
+++ RPG/Graphics/Map/MapRow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG.Graphics.Map
+{
+    class MapRow
+    {
+        public List<MapCell> Columns = new List<MapCell>();
+    }
+}
--- RPG/Graphics/Map/Tile.cs
+++ RPG/Graphics/Map/Tile.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace RPG.Graphics.Map
 {
-    class Tile
+    public static class Tile
     {
+        static public Texture2D TileSetTexture;
+
+        public static Rectangle GetSourceRectangle(int tileIndex)
+        {
+            return new Rectangle(tileIndex * 32, 0, 32, 32);
+        }
     }
 }
